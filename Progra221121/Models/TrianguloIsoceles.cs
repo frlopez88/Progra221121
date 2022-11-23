5725e9c94184bd8942c2b7424b9090fc210b4c3d
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2022-11-22 20:48:03 -0600

    Commit de Triangulo

---
+++ Progra221121/Models/TrianguloIsoceles.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Progra221121.Models
+{
+    public class TrianguloIsoceles : FigurasGeometricas
+    {
+        public double ladoA { get; set; }
+        public double ladoB { get; set; }
+        double altura;
+
+        private void calcularAltura() {
+            double tmp = Math.Pow(ladoA, 2) + (Math.Pow(ladoB, 2) / 4.00);
+            altura = Math.Sqrt(tmp);
+        }
+
+        public new void calcularArea() {
+            calcularAltura();
+
+            area = (ladoB * altura) / 2.00;
+
+        }
+
+        public new void calcularPerimetro() {
+
+            perimetro = (ladoA * 2.00) + ladoB;
+        }
+
+    }
+}
---
+++ Progra221121/ViewModels/ViewModelTriangulo.cs
@@ -0,0 +1,93 @@
+﻿using Progra221121.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Progra221121.ViewModels
+{
+    public class ViewModelTriangulo : INotifyPropertyChanged
+    {
+
+        public ViewModelTriangulo() {
+
+            crearTriangulo = new Command(
+
+                    () => {
+
+                        TrianguloIsoceles t1 = new TrianguloIsoceles() { 
+                            
+                            ladoA = this.ladoA, 
+                            ladoB = this.LadoB
+                        
+                        };
+
+                        t1.calcularArea();
+                        t1.calcularPerimetro();
+
+                        Resultado = t1.toString();
+                    
+                    }
+                
+                );
+
+
+        }
+
+
+        double ladoB;
+
+        public double LadoB
+        {
+            get => ladoB;
+            set
+            {
+
+                ladoB = value;
+                var args = new PropertyChangedEventArgs(nameof(LadoB)); // La Variable public 
+                // La linea de codigo previa, declara una variable de argumentos, cuando un objeto 
+                // dentro de un front end, percible un cambio por medio de un evento (Clic, Drag, Input)
+                PropertyChanged?.Invoke(this, args);
+
+            }
+        }
+
+
+        double ladoA;
+
+        public double LadoA
+        {
+            get => ladoA;
+            set
+            {
+
+                ladoA = value;
+                var args = new PropertyChangedEventArgs(nameof(LadoA)); // La Variable public 
+                // La linea de codigo previa, declara una variable de argumentos, cuando un objeto 
+                // dentro de un front end, percible un cambio por medio de un evento (Clic, Drag, Input)
+                PropertyChanged?.Invoke(this, args);
+
+            }
+        }
+
+        string resultado;
+
+        public string Resultado
+        {
+            get => resultado;
+
+            set
+            {
+
+                resultado = value;
+                var args = new PropertyChangedEventArgs(nameof(Resultado));
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+
+        public Command crearTriangulo { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
